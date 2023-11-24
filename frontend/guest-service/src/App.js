f1ecf6f8748cdeae82672eb027e1b7110db63366
e_riener <vanessa.riener@mogree.com>
2023-11-24 10:41:07 +0100

    fix frontend

--- frontend/guest-service/src/App.js
+++ frontend/guest-service/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Dashboard/>
     </div>
   );
 }
