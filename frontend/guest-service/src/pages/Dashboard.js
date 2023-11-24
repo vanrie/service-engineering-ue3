76862c0ddf406d8650ddc79e4ad35a983026e272
e_riener <vanessa.riener@mogree.com>
2023-11-24 13:42:01 +0100

    add create event form (wip)

--- frontend/guest-service/src/pages/Dashboard.js
+++ frontend/guest-service/src/pages/Dashboard.js
@@ -7,11 +7,27 @@ import { changeEventParticipation } from '../services/EventsService';
 import { getUserInfo } from '../services/UserService';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
 
 
 function Dashboard() {
     const [events, setEvents] = useState();
     const [userInfo, setUserInfo] = useState();
+    const [newEvent = {
+        name: '',
+        description: '',
+        date: '',
+        type: ''
+    }, setNewEvent] = useState();
+
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const dummyUser = {
         _id: '1',
         firstName: 'Asda',
@@ -29,22 +45,19 @@ function Dashboard() {
         })*/
 
         getAllEvents()
-        
+
 
     }, [])
 
-    function getAllEvents(){
+    function getAllEvents() {
         getEvents().then(events => {
 
-            console.log(events);
             const allEvents = []
             for (const event of events.data) {
                 for (const user of event.participants) {
                     if (dummyUser._id == user) {
-                        console.log('is participant');
                         event.isParticipant = true;
                     } else {
-                        console.log('no participant');
                         event.isParticipant = false;
                     }
                     allEvents.push(event);
@@ -65,6 +78,10 @@ function Dashboard() {
         })
     }
 
+    function createNewEvent(event) {
+        console.log(newEvent)
+    }
+
 
     function createParticipation(event) {
         event.participants.push(dummyUser._id);
@@ -74,19 +91,62 @@ function Dashboard() {
     }
 
     function deleteEventById(eventId) {
-        console.log(eventId);
-
         deleteEvent(eventId);
     }
-
     return (
         <div className="dashboard">
-            <div class="header-wrapper ">
+            <div className="header-wrapper ">
                 <h1>Event Service</h1>
                 <h3>Immer bestens informiert!</h3>
                 <p> At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
-                {dummyUser.isAdmin ? <Button variant="primary" onClick={() => {  }}>Event erstellen</Button> : null }
+                {dummyUser.isAdmin ? <Button variant="primary" onClick={() => { handleShow() }}>Event erstellen</Button> : null}
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Neues Event erstellen</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" name="name" placeholder="Name" value={newEvent.name}
+                                onChange={e => setNewEvent({ name: e.target.value })} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Beschreibung</Form.Label>
+                            <Form.Control as="textarea" name="description" rows={3} placeholder="Beschreibung einfügen"
+                                value={newEvent.description}
+                                onChange={e => setNewEvent({ description: e.target.value })} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Datum</Form.Label>
+                            <Form.Control type="date" name="date" placeholder="Datum"
+                                value={newEvent.date}
+                                onChange={e => setNewEvent(newEvent.date = e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Art der Veranstaltung</Form.Label>
+                            <Form.Select value={newEvent.type}
+                                onChange={e => setNewEvent({ type: e.target.value })}>
+                                <option>Konzert</option>
+                                <option>Sport</option>
+                                <option>Messe</option>
+                                <option>Sonstiges</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Form>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={createNewEvent} type="submit">
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
             <div className="list-wrapper">
                 {events?.map((event) => (
