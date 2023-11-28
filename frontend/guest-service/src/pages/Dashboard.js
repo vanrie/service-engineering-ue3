d10e5d853cc56a471694efe79cf38bf71edb0e57
e_riener <vanessa.riener@mogree.com>
2023-11-28 20:30:48 +0100

    add createId

--- frontend/guest-service/src/pages/Dashboard.js
+++ frontend/guest-service/src/pages/Dashboard.js
@@ -97,8 +97,15 @@ function Dashboard() {
     }
 
     function createId(){
-        //TODO Vanessa: create real id
-        return '237892437';
+        let result = '';
+        const characters = 'abcdef0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < 32) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          counter += 1;
+        }
+        return result;
     }
 
 
